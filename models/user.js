a2f354f17dfac4dd7df2eb7366b042fc9361b54c
Andrey Lebedev <alebedev424@gmail.com>
2023-06-01 11:37:00 +0300

    feater: добавил мейл и пароль для пользователя, сделал описание не обязательным

--- models/user.js
+++ models/user.js
@@ -2,25 +2,39 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Поле "email" должно быть заполнено'],
+    unique: [true, 'Поле "unique" должно быть уникальным'],
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Некорректный Email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле "password" должно быть заполнено'],
+    minlength: 8,
+  },
   name: {
     type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: [true, 'Поле "about" должно быть заполнено'],
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator: (v) => validator.isURL(v),
       message: 'Некорректный URL',
     },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 }, { versionKey: false });
 
