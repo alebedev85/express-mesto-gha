8ed86768b918e2428263df30a75b607f9d88a439
Andrey Lebedev <alebedev424@gmail.com>
2023-06-13 15:26:08 +0300

    feater: добавил запись логов

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
@@ -13,7 +15,12 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
+
 app.use(errors());
 app.use(errorHandler);
 
