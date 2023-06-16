119971d3da9bf4310f0d3626a0c505ccbcc5d9fb
Andrey Lebedev <alebedev424@gmail.com>
2023-06-16 11:00:22 +0300

    refactor

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const app = express();
 
 //   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
-//   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//    Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
 //   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 //   const requestHeaders = req.headers['access-control-request-headers'];
 //   if (method === 'OPTIONS') {
@@ -50,7 +50,7 @@ app.use(express.json());
 
 app.use(requestLogger);
 
-app.use(cors());
+// app.use(cors());
 
 app.use(router);
 
